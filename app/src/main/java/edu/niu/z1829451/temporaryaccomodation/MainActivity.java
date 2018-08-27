e981c98d8cfbf9734e052fc61b097d25be90d195
sivalingam77 <lingams200@gmail.com>
2018-08-27 13:29:04 -0500

    Initial commit

---
+++ app/src/main/java/edu/niu/z1829451/temporaryaccomodation/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.niu.z1829451.temporaryaccomodation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

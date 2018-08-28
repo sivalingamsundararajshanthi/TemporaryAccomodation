096ce78285419516d8895ce799dd9d4e86f2fd31
sivalingam77 <lingams200@gmail.com>
2018-08-27 20:46:24 -0500

    Finish authentication

--- app/src/main/java/edu/niu/z1829451/temporaryaccomodation/MainActivity.java
+++ app/src/main/java/edu/niu/z1829451/temporaryaccomodation/MainActivity.java
@@ -1,13 +1,65 @@
 package edu.niu.z1829451.temporaryaccomodation;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String ANONYMOUS = "anonymous";
+    private static final int RC_SIGN_IN = 123;
+
+    private String mUserName;
+    private FirebaseAuth mFireBaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFireBaseAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    mUserName = user.getDisplayName();
+                    Log.d("USERNAME", mUserName);
+                } else {
+                    mUserName = ANONYMOUS;
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(false)
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                    new AuthUI.IdpConfig.EmailBuilder().build()))
+                            .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFireBaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mAuthStateListener!=null)
+            mFireBaseAuth.removeAuthStateListener(mAuthStateListener);
     }
 }
